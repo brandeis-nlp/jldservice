7bf21ca390742ec3720e862088938bdf064c5737
chunqishi <shicq@cs.brandeis.edu>
2014-03-29 13:22:06 -0400

    Test configuration failure

--- src/test/groovy/org/jldservice/config/TestConfig.groovy
+++ src/test/groovy/org/jldservice/config/TestConfig.groovy
@@ -24,7 +24,8 @@ class TestConfig{
     public void test() {
         def config = new Config("/jar.config");
         Assert.assertEquals(config.get("path.libs").trim(), "target") ;
-        Assert.assertEquals(config.get("git.urls").trim(), "http://github.com/chunqishi") ;
+        Assert.assertEquals(config.get("git.urls").trim(), "https://github.com/chunqishi/edu.brandeis.cs.opennlp-web-service.git\n" +
+                "        https://github.com/chunqishi/edu.brandeis.cs.stanfordnlp-web-service.git") ;
     }
 
 }
