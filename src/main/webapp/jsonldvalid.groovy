c92863e18d7a6b6832d1ed6d298a8bf2803801e6
chunqishi <shicq@cs.brandeis.edu>
2014-03-29 13:10:42 -0400

    added json validation

--- src/main/webapp/jsonldvalid.groovy
+++ src/main/webapp/jsonldvalid.groovy
@@ -26,41 +26,103 @@ println """
 
 <script type="text/javascript">
 
-function compact() {
-    var doc = \$("#jsonld_doc").val();
-    var context = \$("#jsonld_context").val();
-
-    jsonld.compact(jQuery.parseJSON(doc), jQuery.parseJSON(context), function(err, compacted) {
-        console.log(JSON.stringify(compacted, null, 2));
-        \$("#jsonld_compacted").val(JSON.stringify(compacted, null, 2));
+function display(id) {
+    var json = \$("#" + id).val();
+    if (json) {
         var node = new PrettyJSON.view.Node({
-                el:\$("#jsonld_compacted_pretty"),
-                data:compacted
+                el:\$("#display_" + id),
+                data:jQuery.parseJSON(json)
         });
-    });
+        node.expandAll();
+    }
 }
-
 
 function valid() {
     var compact_0 = \$("#jsonld_compact_0").val();
-    jsonld.expand(jQuery.parseJSON(compact_0), function(err, expanded) {
-        console.log(JSON.stringify(expanded, null, 2));
-        \$("#jsonld_expanded_0").val(JSON.stringify(expanded, null, 2));
-    });
+    \$("#jsonld_expanded_0").val("");
+
+    var json_compact_0;
+    try
+    {
+      json_compact_0 = jQuery.parseJSON(compact_0)
+    } catch(err) {
+      \$("#jsonld_expanded_0").attr("placeholder", "Compacted -->  " + err);
+    }
+    if (json_compact_0) {
+        jsonld.expand(json_compact_0, function(err, expanded) {
+            console.log(JSON.stringify(expanded, null, 2));
+            if(err) {
+                \$("#jsonld_expanded_0").attr("placeholder", "Expand -->  " + JSON.stringify(err, null, 2));
+            }
+            if (expanded) {
+                \$("#jsonld_expanded_0").val(JSON.stringify(expanded, null, 2));
+            }
+            display("jsonld_compact_0");
+            display("jsonld_expanded_0");
+        });
+    }
 }
+
+function compact() {
+    var doc = \$("#jsonld_doc").val();
+    var context = \$("#jsonld_context").val();
+    \$("#jsonld_compacted").val("");
+
+    var json_doc;
+    var json_context;
+    try
+    {
+      json_doc = jQuery.parseJSON(doc)
+    } catch(err) {
+      \$("#jsonld_compacted").attr("placeholder", "Document -->  " + err);
+    }
+
+        var json_compact_0;
+    try
+    {
+      json_context = jQuery.parseJSON(context)
+    } catch(err) {
+      \$("#jsonld_compacted").attr("placeholder", "Context -->  " + err);
+    }
+
+    if (json_doc && json_context) {
+        jsonld.compact(json_doc, json_context, function(err, compacted) {
+            console.log(JSON.stringify(compacted, null, 2));
+            if (err) {
+                \$("#jsonld_compacted").attr("placeholder",  "Compact -->  " + JSON.stringify(err, null, 2));
+            }
+            if (compacted) {
+                \$("#jsonld_compacted").val(JSON.stringify(compacted, null, 2));
+            }
+            display("jsonld_doc");
+            display("jsonld_context");
+            display("jsonld_compacted");
+        });
+    }
+}
+
+
+
 </script>
 
 </head>
 
 <body>
-    <h2> Please valid the Json </h2>
+    <h1  align="center">JSON Validate</h1>
+
+    <h2> Please Validate the Json </h2>
     <iframe src="http://pro.jsonlint.com/" seamless="seamless" width="62%" scrolling="yes">
     <p>Your browser does not support iframes, please goto <a href="http://pro.jsonlint.com/" target="_blank">JsonLint</a>.</p>
     </iframe>
     <p>
+    <!------------------------------------------------------------------------------------>
     <hr/>
-    <h2> Please valid the Json-LD</h2>
-    <table><tr><td>
+    <h2> Please Validate the Json-LD</h2>
+    <table>
+
+    <tr>
+    <th>Compacted: </th><th></th><th> Expanded:</th>
+    </tr><tr><td>
     <textarea id="jsonld_compact_0" cols="50" rows="20">{
   "@context": {
     "name": "http://schema.org/name",
@@ -78,15 +140,17 @@ function valid() {
   "homepage": "http://manu.sporny.org/"
 }</textarea></td><td>
    <input type="button" value="Valid" onclick="valid()" /></td><td>
-   <textarea id="jsonld_expanded_0" cols="50" rows="20"></textarea></td></tr>
+   <textarea id="jsonld_expanded_0" cols="50" rows="20"></textarea></td></tr><tr><td>
+   <div id="display_jsonld_compact_0"></div></td><td></td><td><div id="display_jsonld_expanded_0"></div></td></tr>
     </table>
+    <!------------------------------------------------------------------------------------>
     <hr/>
-    <h2> Please compact the Json-LD</h2>
+    <h2> Please Compact the Json-LD</h2>
 
     <p>
     <table>
     <tr>
-    <th>Document: </th><th> Context:</ht>
+    <th>Document: </th><th> Context:</th><th> </th><th>Compacted Json-LD</th>
     </tr><tr><td>
     <textarea id="jsonld_doc" cols="50" rows="20">{
   "http://schema.org/name": "Manu Sporny",
@@ -97,16 +161,14 @@ function valid() {
   "name": "http://schema.org/name",
   "homepage": {"@id": "http://schema.org/url", "@type": "@id"},
   "image": {"@id": "http://schema.org/image", "@type": "@id"}
-}</textarea></td><tr/>
-    <tr>
-    <td colspan="2">
-    <p><input type="button" value="Compact" onclick="compact()" />
-    <p><b>Compacted Json-LD</b></p>
-    </td></tr><tr><td>
+}</textarea></td><td>
+    <input type="button" value="Compact" onclick="compact()" />
+    </td><td>
     <textarea  id="jsonld_compacted"  cols="50" rows="20"> </textarea>
-    </td><td><span id="jsonld_compacted_pretty"></span>
-    </td>
-    </tr>
+    </td></tr><tr><td>
+    <div id="display_jsonld_doc"></div></td><td><div id="display_jsonld_context"></div></td><td></td>
+    <td><span id="display_jsonld_compacted"></span>
+    </td></tr>
     </table>
 
 </body>
